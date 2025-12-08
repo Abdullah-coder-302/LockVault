60b56176b960a3c5927b4a02fb0805023964a5a5
Abdullah <abdullahrajpoot302123@gmail.com>
2025-12-08 21:27:53 +0500

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <footer className="bg-black  text-white text-center   opacity-80" style={{padding: "16px"}}>
+        <p>© 2025 PassVault. All rights reserved.</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
